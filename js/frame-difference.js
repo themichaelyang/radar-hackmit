914caa59ef1f7df717b3d17e4202bf02d7f9d64d
Michael Yang <themichaelyang@gmail.com>
2016-09-17 17:56:35 -0400

    added new frame differencing function

---
+++ js/frame-difference.js
@@ -0,0 +1,14 @@
+function frameDifference(currentFrame, previousFrame) { // takes canvas with frames
+  let currentFrameImageData = currentFrame.getImageData(0, 0, currentFrame.width, currentFrame.height);
+  for (let i = 0; i < currentFrameImageData.length; i += 4) {
+
+  }
+}
+
+function indexToCoordinates(index, width, height) {
+
+}
+
+function coordinatesToIndex(coordinates, width, height) { // coordinates is obj {x, y}
+
+}
