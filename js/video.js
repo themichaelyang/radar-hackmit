9dc9863b47cc85a172e2ad645efc4897ab8ed981
Michael Yang <themichaelyang@gmail.com>
2016-09-17 14:28:46 -0400

    created video stream

--- js/video.js
+++ js/video.js
@@ -1,19 +1,25 @@
-function getVideo() {
+// 'use strict'
 
-  let videoConstraints = {
-    video: { width: 480, height: 270, facingMode: 'user' }
-    audio: false,
+function getVideo() {
+  // consider setting the video constraints in the individual video media track within the stream
+  let video = document.createElement('video');
+  let userMediaConstraints = {
+    video: { width: 480, height: 270, facingMode: 'user' },
+    audio: false
   };
 
-  navigator.mediaDevices.getUserMedia(videoConstraints)
+  navigator.mediaDevices.getUserMedia(userMediaConstraints)
                         .then(onGetUserMediaSuccess)
                         .catch(onGetUserMediaError);
-}
 
-function onGetUserMediaSuccess(mediaStream) {
+  function onGetUserMediaSuccess(mediaStream) {
+    video.src = window.URL.createObjectURL(mediaStream);
+    video.play();
+  }
 
-}
+  function onGetUserMediaError(error) {
+    console.error(error);
+  }
 
-function onGetUserMediaError(error) {
-
+  document.body.appendChild(video);
 }
