2d295891ec1c2e664fa1eb71f4c7aa62a1582fcc
mjqzhang <mikejqzhang.2@gmail.com>
2016-09-17 15:20:21 -0400

    cleaned it up

--- js/ImageCompare.js
+++ js/ImageCompare.js
@@ -2,66 +2,58 @@
 
 "use strict";
 
-(function {
+function ImageCompare() {
 
 
-	App.ImageCompare = function() {
-        const sensitivity
-        let tempCanvas1, tempCanvas2
-        let height, width
+    let sensitivity = 30;
 
-        function compare(tempCanvas1, tempCanvas2, sensitivity) {
+    function compare(tempCanvas1, tempCanvas2, width, height) {
 
-            tempCanvas1.drawImage(image1, 0, 0, width, height);
-            tempCanvas2.drawImage(image2, 0, 0, width, height);
+        tempCanvas1.drawImage(image1, 0, 0, width, height);
+        tempCanvas2.drawImage(image2, 0, 0, width, height);
 
-            let sumx = 0;
-            let sumy = 0;
-            let changes = 0;
+        let sumx = 0;
+        let sumy = 0;
+        let changes = 0;
 
-            for (let x = 0; x < width; x ++) {
-                for (let y = 0; y < height; y ++) {
-                    let pixel1 = getImageData(x, y, 1, 1,)
-                    let pixel2 = getImageData(x, y, 1, 1,)
+        for (let x = 0; x < width; x ++) {
+            for (let y = 0; y < height; y ++) {
+                let pixel1 = getImageData(x, y, 1, 1);
+                let pixel2 = getImageData(x, y, 1, 1);
 
-                    if (comparepixel(pixel1.data, pixel2.data)) {
-                        sumx += x;
-                        sumy += y;
-                        changes += 1;
-                    }
+                if (comparepixel(pixel1.data, pixel2.data)) {
+                    sumx += x;
+                    sumy += y;
+                    changes += 1;
                 }
-            }
-
-            let location = {
-                let x = sumx / changes
-                let y = sumy / changes
-            }
-
-            return location
-
-
-        function comparepixel(p1data, p2data, sensitivity) {
-
-            let diff = false
-            let dr, dg, db;
-            dr = (p1data[0] - p2data[0]);
-            dg = (p1data[1] - p2data[1]);
-            db = (p1data[2] - p2data[2]);
-
-            distance = Math.sqrt( dr * dr + dg * dg + db * db );
-
-            if (distance < sensitivity) {
-                diff = true
-            }
-            return diff
-
             }
         }
 
+        let location = [sumx / changes, sumy / changes]
+
+        return location
+
+
+    function comparepixel(p1data, p2data) {
+
+        let diff = false;
+        let dr, dg, db;
+        dr = (p1data[0] - p2data[0]);
+        dg = (p1data[1] - p2data[1]);
+        db = (p1data[2] - p2data[2]);
+
+        distance = Math.sqrt( dr * dr + dg * dg + db * db );
+
+        if (distance < sensitivity) {
+            diff = true;
+        }
+        return diff
+
+        }
     }
+
 
     return {
         compare: compare
     }
-
-})();
+};
